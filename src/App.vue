e8e55e1c8185557352f5f6b2488882789d9a006e
jaiselrahman <jaiselrahman@gmail.com>
2019-08-10 14:00:28 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    Hello World
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "App",
+  components: {
+  },
+  data() {
+    return { };
+  },
+  methods: {
+  }
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Itim");
+
+html {
+  scroll-behavior: smooth;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: "Itim";
+}
+</style>
