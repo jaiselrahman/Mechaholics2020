1493a18741c7e4931fdc8bf9cd3a6251a1897dff
jaiselrahman <jaiselrahman@gmail.com>
2019-08-11 19:59:00 +0530

    Set up github actions for deployment

---
+++ .github/actions/deploy/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -l
+set -e
+
+if [ -z "$FIREBASE_TOKEN" ]
+then
+  echo "You must provide Firebase token"
+  exit 1
+fi
+
+echo "Installing firebase-tools"
+npm install -g firebase-tools
+
+echo "Creating firebaseConfig.js file"
+echo $FIREBASE_CONFIG >> src/firebaseConfig.js
+
+echo "Installing npm dependencies"
+npm install
+
+echo "Build production version"
+npm run build --modern
+
+echo "Deploying to Firebase hosting"
+firebase --debug deploy --only hosting
+
+echo "Deployment succesful!"
---
+++ .github/main.workflow
@@ -0,0 +1,11 @@
+workflow "Build and deploy on push" {
+  on = "push"
+  resolves = [
+    "Deploy",
+  ]
+}
+
+action "Deploy" {
+  uses = "./.github/actions/deploy"
+  secrets = ["FIREBASE_TOKEN", "FIREBASE_CONFIG"]
+}
