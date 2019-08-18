5da94da8b8470335b5a88a4c78a39e15a94b8418
Jaisel Rahman <jaiselrahman@gmail.com>
2019-08-18 15:57:09 +0530

    Validate registration detail

--- src/components/RegistrationPage.vue
+++ src/components/RegistrationPage.vue
@@ -1,24 +1,26 @@
 <template>
   <div id="register">
-    <form
-      class="form"
-      method="post"
-      action="https://script.google.com/macros/s/AKfycbybm4JNQcwQJNuwnjiIfJzFBv3Uxizj1zrbGqtDuUrmZf3FPmVn/exec"
-    >
+    <form class="form" @submit.prevent="submit">
       <label class="label" for="Name">Name</label>
-      <input class="field" type="text" name="Name" required />
-      <br />
+      <input class="field" type="text" v-model="name" name="Name">
+      <div class="error" v-if="$v.$error && !$v.name.required">Name is required</div>
+      <br>
       <label class="label" for="Email">Email</label>
-      <input class="field" type="text" name="Email" required />
-      <br />
+      <input class="field" type="text" v-model="email" name="Email">
+      <div class="error" v-if="$v.$error && !$v.email.required">Email is required</div>
+      <div class="error" v-else-if="$v.$error && !$v.email.email">Enter valid email</div>
+      <br>
       <label class="label" for="Phone">Phone</label>
-      <input class="field" type="text" name="Phone" required />
-      <br />
+      <input class="field" type="text" v-model="phone" name="Phone">
+      <div class="error" v-if="$v.$error && !$v.phone.required">Phone is required</div>
+      <div class="error" v-if="$v.$error && !($v.phone.numeric && $v.phone.isValidPhone)">Enter valid phone number</div>
+      <br>
       <label class="label" for="College">College</label>
-      <input class="field" type="text" name="College" required />
-      <br />
+      <input class="field" type="text" v-model="college" name="College">
+      <div class="error" v-if="$v.$error && !$v.college.required">College is required</div>
+      <br>
       <label class="label" for="Department">Department</label>
-      <select class="field" name="Department" required>
+      <select class="field" name="Department" v-model="department">
         <option disabled>Select Department</option>
         <option>B.Tech. IT</option>
         <option>B.E. CSE</option>
@@ -26,25 +28,76 @@
         <option>B.C.A/M.C.A</option>
         <option>Others</option>
       </select>
-      <br />
-      <label class="label" for="Person Count">Person Count</label>
-      <input class="field" type="text" name="Person Count" required />
-      <br />
+      <div class="error" v-if="$v.$error && !$v.department.required">Select your department</div>
+      <br>
       <label class="label" for="Food Preference">Food Preference</label>
-      <select class="field" name="Food Preference" required>
+      <select class="field" name="Food Preference" v-model="foodPreference">
         <option disabled>Veg./Non Veg.</option>
         <option>Veg</option>
         <option>Non Veg</option>
       </select>
-      <br />
-      <input class="register-button" type="submit" value="Register" />
+      <div class="error" v-if="$v.$error && !$v.foodPreference.required">Select food preference</div>
+      <br>
+      <div v-if="submitStatus === 'OK'">Thanks for your registration!</div>
+      <button class="register-button" type="submit" :disabled="submitStatus === 'PENDING'">Register</button>
     </form>
   </div>
 </template>
 
 <script>
+import { required, email, numeric} from "vuelidate/lib/validators";
+
 export default {
-  name: "RegistrationPage"
+  name: "RegistrationPage",
+  data() {
+    return {
+      name: "",
+      email: "",
+      phone: "",
+      college: "",
+      department: "",
+      foodPreference: "",
+      submitStatus: ""
+    };
+  },
+  validations: {
+    name: {
+      required
+    },
+    email: {
+      required,
+      email
+    },
+    phone: {
+      required,
+      numeric,
+      isValidPhone(phone) {
+        return !!phone && phone.length === 10;
+      }
+    },
+    college: {
+      required
+    },
+    department: {
+      required
+    },
+    foodPreference: {
+      required
+    }
+  },
+  methods: {
+    submit() {
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        this.submitStatus = "ERROR";
+      } else {
+        this.submitStatus = "PENDING";
+        setTimeout(() => {
+          this.submitStatus = "OK";
+        }, 500);
+      }
+    }
+  }
 };
 </script>
 
@@ -87,6 +140,10 @@ option {
   border: none;
 }
 
+.error {
+  color: #ef5350;
+}
+
 .register-button {
   width: 30%;
   background: #ef535054;
@@ -94,11 +151,16 @@ option {
   font-size: 1.2rem;
   outline: none;
   border: none;
+  cursor: pointer;
   border-radius: 2rem;
   color: white;
   margin: 0.5rem 0;
   background: #ef5350;
   box-shadow: 0px 0px 0.6rem 0.1rem rgba(0, 0, 0, 0.5);
+}
+
+.register-button:active {
+  transform: scale(0.98);
 }
 
 @media only screen and (max-width: 900px) {
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import Vuelidate from 'vuelidate'
 import App from './App.vue'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuelidate)
 
 new Vue({
   render: h => h(App)
