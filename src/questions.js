397f3d49ae666dd358ee27dafe1c9dd656c7e173
Jaisel Rahman <jaiselrahman@gmail.com>
2019-08-29 22:29:39 +0530

    Add basic google-it page

---
+++ src/questions.js
@@ -0,0 +1,61 @@
+export default [
+    {
+        questions: [{
+            type: "html",
+            html: "You are about to start the Quiz.<br/>You have 60 seconds for every questions.<br/>Please click on <b>'Start Quiz'</b> button when you are ready."
+        }]
+    },
+    {
+        questions: [{
+            type: "text",
+            name: "question1",
+            title: 'Question 1 ?',
+            correctAnswer: 'answer',
+        }]
+    }, {
+        questions: [{
+            type: "text",
+            name: "question2",
+            title: 'Question 2 ?',
+            correctAnswer: 'answer',
+        }]
+    }, {
+        questions: [{
+            type: "text",
+            name: "question3",
+            title: 'Question 3 ?',
+            correctAnswer: 'answer',
+        }]
+    }, {
+        questions: [{
+            type: "text",
+            name: "question4",
+            title: 'Question 4 ?',
+            correctAnswer: 'answer',
+        }]
+    },
+    {
+        questions: [{
+            type: "text",
+            name: "question5",
+            title: 'Question 5 ?',
+            correctAnswer: 'answer',
+        }]
+    },
+    {
+        questions: [{
+            type: "text",
+            name: "question",
+            title: 'Question 6 ?',
+            correctAnswer: 'answer',
+        }]
+    },
+    {
+        questions: [{
+            type: "text",
+            name: "question7",
+            title: 'Question 7 ?',
+            correctAnswer: 'answer',
+        }]
+    },
+]
--- src/views/GoogleIt.vue
+++ src/views/GoogleIt.vue
@@ -1,13 +1,61 @@
 <template>
-    <h2>Google It</h2>
+  <div class="google-it">
+    <survey :survey="survey"></survey>
+  </div>
 </template>
 
 <script>
-export default {
+import questions from "@/questions";
 
-}
+import * as SurveyVue from "survey-vue";
+import "bootstrap/dist/css/bootstrap.css";
+
+var Survey = SurveyVue.Survey;
+Survey.cssType = "bootstrap";
+
+export default {
+  name: "GoogleIt",
+  components: {
+    Survey
+  },
+  data() {
+    var json = {
+      title: "Google It",
+      showProgressBar: "top",
+      showTimerPanel: "top",
+      maxTimeToFinishPage: 60,
+      firstPageIsStarted: true,
+      startSurveyText: "Start Quiz",
+      pages: questions,
+      completedHtml: "<h4 style=\"text-align:center\">You have answered correctly <b>{correctedAnswers}</b> questions from <b>{questionCount}</b>.</h4>"
+    };
+    var model = new SurveyVue.Model(json);
+    return {
+      survey: model,
+    };
+  },
+  mounted() {
+    this.survey.onComplete.add(() => {
+    //   this.result = `Your score: ${this.survey.getCorrectedAnswerCount()}/${
+    //     this.survey.getQuizQuestions().length
+    //   }`;
+    });
+
+    this.survey.onTimerPanelInfoText.add((sender, options) => {
+      options.text = `Remining Time: ${sender.maxTimeToFinishPage -
+        sender.currentPage.timeSpent}s`;
+    });
+  }
+};
 </script>
 
 <style>
+.google-it {
+  color: black;
+}
 
+.progress + div {
+    margin-left: 1rem;
+    font-weight: bold; 
+}
 </style>
