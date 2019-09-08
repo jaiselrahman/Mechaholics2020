f05aad4339506ba008d7f3cdb924877be0668899
Jaisel Rahman <jaiselrahman@gmail.com>
2019-09-08 17:20:26 +0530

    Update question set

--- src/questions.js
+++ src/questions.js
@@ -8,54 +8,78 @@ export default [
     {
         questions: [{
             type: "text",
-            name: "question1",
-            title: 'Question 1 ?',
-            correctAnswer: 'answer',
+            name: "Error",
+            title: 'Name a 5 letter word which has three consonants all the same and two different vowels. Every now and then you see this while running a windows 95/98 on your PC.',
+            correctAnswer: 'Error',
         }]
     }, {
         questions: [{
             type: "text",
-            name: "question2",
-            title: 'Question 2 ?',
-            correctAnswer: 'answer',
+            name: "Big Mac",
+            title: 'What do you get when you cross a hamburger with a computer ?',
+            correctAnswer: 'Big Mac',
         }]
     }, {
         questions: [{
             type: "text",
-            name: "question3",
-            title: 'Question 3 ?',
-            correctAnswer: 'answer',
+            name: "Firewall",
+            title: 'I am a Barrier that acts as a security system to protect trusted computer system. Who am I ?',
+            correctAnswer: 'Firewall',
         }]
     }, {
         questions: [{
             type: "text",
-            name: "question4",
-            title: 'Question 4 ?',
-            correctAnswer: 'answer',
+            name: "Intranet",
+            title: 'Basically a private internal internet specific to an organisation or a group. Who am I ?',
+            correctAnswer: 'Intranet',
         }]
     },
     {
         questions: [{
             type: "text",
-            name: "question5",
-            title: 'Question 5 ?',
-            correctAnswer: 'answer',
+            name: "Computer Disc",
+            title: 'I come in square package but I am round, I contain lots of Information for your Computer, handle me carefully, What am I ?',
+            correctAnswer: 'Compact Disc',
         }]
     },
     {
         questions: [{
             type: "text",
-            name: "question",
-            title: 'Question 6 ?',
-            correctAnswer: 'answer',
+            name: "Home Page",
+            title: 'You can see me usually at the starting point of an organisation website. Who am I ?',
+            correctAnswer: 'Home Page',
         }]
     },
     {
         questions: [{
             type: "text",
-            name: "question7",
-            title: 'Question 7 ?',
-            correctAnswer: 'answer',
+            name: "Saas",
+            title: 'I am a software distribution model where software applications are centrally hosted and licensed on subscription basis. Who am I ?',
+            correctAnswer: 'Saas',
+        }]
+    },
+    {
+        questions: [{
+            type: "text",
+            name: "ACCRINTM",
+            title: 'Find the MS Excel function to return accrued interest for a security that pays interest at maturity is',
+            correctAnswer: 'ACCRINTM',
+        }]
+    },
+    {
+        questions: [{
+            type: "text",
+            name: "C++",
+            title: 'My name has a post Increment but not Preincrement. Every Software Engineer knows my name',
+            correctAnswer: 'C++',
+        }]
+    },
+    {
+        questions: [{
+            type: "text",
+            name: "Pointer",
+            title: "I provide way to find some other, my name ends with 'R', I am not a palindrome find me who I am ?",
+            correctAnswer: 'Pointer',
         }]
     },
 ]
